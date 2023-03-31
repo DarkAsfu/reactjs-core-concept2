2138e4a6b84f4c4aabcb358512f2f7ca8a193d7f
DarkAsfu <ashraful.islam.asfu@gmail.com>
2023-03-31 14:29:25 +0600

    State, component state hook and sete state method

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  const products = [
-    {name: 'laptop', price: 63000},
-    {name: 'phone', price: 73000},
-    {name: 'watch', price: 3000},
-    {name: 'tablet', price: 43000},
-  ]
+  
   return (
     <div className="App">
-      {/* <Product name="Laptop" price="63000"></Product>
-      <Product name="Phone" price="75000"></Product>
-      <Product name="Watch" price="5000"></Product> */}
-      {
-        products.map(product => <Product name={product.name} price={product.price} ></Product>)
-      }
+      <Counter></Counter>
     </div>
   );
 }
-function Product(props) {
+function Counter() {
+  const [count, setCount] = useState(1);
+  const incraseCount = ()=>{
+    setCount(count+1);
+  }
+  const decreaseCount = () => setCount(count-1);
+  
+  // const incraseCount = ()=>{
+  //   const newCount = count+1;
+  //   setCount(newCount);
+  // } 
   return (
-    <div className='product'>
-      <h3>Name: {props.name}</h3>
-      <p>Price: {props.price}</p>
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={incraseCount}>Increase</button>
+      <button onClick={decreaseCount}>Decrease</button>
     </div>
   )
 }
