ea34088f15f2a223b6baac330bcfe04d327b3cb2
DarkAsfu <ashraful.islam.asfu@gmail.com>
2023-03-31 12:24:44 +0600

    Concept Recap, JSX, components, props, display array of objects

--- src/App.js
+++ src/App.js
@@ -2,24 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const products = [
+    {name: 'laptop', price: 63000},
+    {name: 'phone', price: 73000},
+    {name: 'watch', price: 3000},
+    {name: 'tablet', price: 43000},
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Product name="Laptop" price="63000"></Product>
+      <Product name="Phone" price="75000"></Product>
+      <Product name="Watch" price="5000"></Product> */}
+      {
+        products.map(product => <Product name={product.name} price={product.price} ></Product>)
+      }
     </div>
   );
 }
-
+function Product(props) {
+  return (
+    <div className='product'>
+      <h3>Name: {props.name}</h3>
+      <p>Price: {props.price}</p>
+    </div>
+  )
+}
 export default App;
